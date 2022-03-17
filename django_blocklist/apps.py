a5bafc477cd1916d07a17488639d8b60e98784bf
Paul Bissex <paul@bissex.net>
2022-03-16 21:10:22 -0400

    Fixed app name

--- django_blocklist/apps.py
+++ django_blocklist/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BlocklistConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "blocklist"
+    name = "django_blocklist"
