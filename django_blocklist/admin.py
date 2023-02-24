622aa3e560420172ebf8211334b10a167a13919d
Paul Bissex <paul@bissex.net>
2023-02-23 21:10:06 -0500

    Added 'days left' to admin list, and added some type annotations; closes #16

--- django_blocklist/admin.py
+++ django_blocklist/admin.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib import admin
 from django.http import HttpResponseRedirect
 
@@ -5,13 +7,22 @@ from .models import BlockedIP
 
 
 @admin.display(description="Reason")
-def reason_truncated(entry):
+def reason_truncated(entry: BlockedIP) -> str:
     return entry.reason[:20] + ("..." if len(entry.reason) > 20 else "")
 
 
 @admin.display(description="Cooldown")
-def cooldown(entry):
+def cooldown(entry: BlockedIP) -> str:
     return f"{entry.cooldown} days"
+
+
+@admin.display(description="Days left")
+def days_left(entry: BlockedIP) -> str:
+    if entry.last_seen:
+        remaining = f"{entry.cooldown - (datetime.datetime.today() - entry.last_seen).days}"
+    else:
+        remaining = ""
+    return remaining
 
 
 @admin.action(permissions=["view"])
@@ -21,7 +32,7 @@ def look_up_first_selected_IP(modeladmin, request, queryset):
 
 
 class BlockedIPAdmin(admin.ModelAdmin):
-    list_display = ["ip", "first_seen", "last_seen", "tally", cooldown, reason_truncated]
+    list_display = ["ip", "first_seen", "last_seen", "tally", cooldown, days_left, reason_truncated]
     list_filter = ["first_seen", "last_seen", "cooldown", "reason"]
     search_fields = ["ip", "reason"]
     actions = [look_up_first_selected_IP]
